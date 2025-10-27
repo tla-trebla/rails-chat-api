c498bb20c82fae5bb53c213f00a0d58f671646bb
Albert <stamina.left@gmail.com>
2025-10-28 00:31:04 +0700

    Added implementation on MessagesChannel if it receives a Message data, by broadcasting it to every MessageChannel subscriber

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,6 +1,17 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "MessageChannel"
+    stream_from "MessagesChannel"
+  end
+
+  def receive(data)
+    message = Message.create!(sender_id: data["sender_id"], content: data["content"])
+
+    ActionCable.server.broadcast("MessagesChannel", {
+      id: message.id,
+      sender_id: message.sender_id,
+      content: message.content,
+      created_at: message.created_at.strftime("%Y-%m-%d %H:%M:%S")
+    })
   end
 
   def unsubscribed
